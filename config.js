d33470953f13e32b6e0df2ea06e267b2a1e700ba
maykocarvalho <maykoaraujocarvalho@gmail.com>
2025-08-11 09:12:40 -0300

    Inicial

---
+++ config.js
@@ -0,0 +1,10 @@
+// Preencha com o objeto de configuração do Firebase do seu app Web
+export const firebaseConfig = {
+  apiKey: "AIzaSyCk4uSzsn601D7q1M3e3MZD5XRYTRln0Jc",
+  authDomain: "acionamentocbmgo.firebaseapp.com",
+  databaseURL: "https://acionamentocbmgo-default-rtdb.firebaseio.com",
+  projectId: "acionamentocbmgo",
+  storageBucket: "acionamentocbmgo.firebasestorage.app",
+  messagingSenderId: "805187066238",
+  appId: "1:805187066238:web:0cf2e6ead56ad0a58dbb2f"
+};
